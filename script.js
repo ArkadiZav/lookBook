7307dbe1e09e47344a0d81d7dc2ea4ae89a1d564
Arkadi <mrarkadi@gmail.com>
2017-02-21 16:20:25 +0200

    finished first exercise with both append and handlebars methods

---
+++ script.js
@@ -0,0 +1,43 @@
+// handlebars method:
+var source = $('#lookbook-template').html();
+var template = Handlebars.compile(source);
+
+var fetch = function () {
+  $.ajax({
+    method: "GET",
+    url: 'https://www.googleapis.com/books/v1/volumes?q=isbn:0439023521',
+    dataType: "json",
+    success: function(data) {
+     console.log(data);
+    //  addBook(data);
+      addBook2(data);
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      console.log(textStatus);
+    }
+  });
+};
+
+var clickHandler = function () {
+  fetch();
+}
+
+// append method
+var addBook = function(data) {
+  $('.viewbookdata').append('<h5>' + data.items[0].volumeInfo.description + '</h5>' +
+  '<h3> Written by: ' + data.items[0].volumeInfo.authors[0] +
+  '</h3>' + '<img src = "http://books.google.com/books/content?id=hlb_sM1AN0gC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api" >');
+  };
+
+// handlebars method
+var addBook2 = function (data) {
+  var books = [
+    { author: data.items[0].volumeInfo.authors[0],
+      description: data.items[0].volumeInfo.description,
+      image: "http://books.google.com/books/content?id=hlb_sM1AN0gC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api"
+    }
+  ];
+  console.log(books[0].author);
+  var newHTML = template({description: books[0].description, author: books[0].author, image: books[0].image});
+  $(".viewbookdata").append(newHTML);
+}
