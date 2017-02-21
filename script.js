1222641a194b6a6471361702cc0a92c0a2022a73
Arkadi <mrarkadi@gmail.com>
2017-02-21 17:10:56 +0200

    finished everything not including extensions

--- script.js
+++ script.js
@@ -2,10 +2,11 @@
 var source = $('#lookbook-template').html();
 var template = Handlebars.compile(source);
 
-var fetch = function () {
+var fetch = function (isbn) {
   $.ajax({
     method: "GET",
-    url: 'https://www.googleapis.com/books/v1/volumes?q=isbn:0439023521',
+    url: 'https://www.googleapis.com/books/v1/volumes?q=isbn' + ":" + isbn,
+  //  url: 'https://www.googleapis.com/books/v1/volumes?q=isbn:0439023521',
     dataType: "json",
     success: function(data) {
      console.log(data);
@@ -19,7 +20,9 @@ var fetch = function () {
 };
 
 var clickHandler = function () {
-  fetch();
+  var isbnNum = $("#search").val();
+  $(".form-group")[0].reset();
+  fetch(isbnNum);
 }
 
 // append method
@@ -34,10 +37,10 @@ var addBook2 = function (data) {
   var books = [
     { author: data.items[0].volumeInfo.authors[0],
       description: data.items[0].volumeInfo.description,
-      image: "http://books.google.com/books/content?id=hlb_sM1AN0gC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api"
+      image: data.items[0].volumeInfo.imageLinks.smallThumbnail
     }
   ];
-  console.log(books[0].author);
+  $(".viewbookdata").empty();
   var newHTML = template({description: books[0].description, author: books[0].author, image: books[0].image});
   $(".viewbookdata").append(newHTML);
 }
